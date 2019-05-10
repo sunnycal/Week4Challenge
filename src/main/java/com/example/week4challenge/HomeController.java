9bc7593a024ed3da83ca047c6585e5bfb031828d
Oona Carbonie <carbonie.oona@gmail.com>
2019-05-10 11:29:42 -0400

    third revision on HomeController

--- src/main/java/com/example/week4challenge/HomeController.java
+++ src/main/java/com/example/week4challenge/HomeController.java
@@ -36,6 +36,31 @@ public class HomeController {
     public String coreJavaTwo() {
         return "coreJavaConcept2";
     }
+    @RequestMapping("/Prog&Alg_One")
+    public String programAlgorithOne() {
+        return "program_design_&_algorithms1";
+    }
+    @RequestMapping("/Prog&Alg_Two")
+    public String programAlgorithTwo() {
+        return "program_design_&_algorithms2";
+    }
+    @RequestMapping("/OOP_One")
+    public String oopOne() {
+        return "oopConcept1";
+    }
+    @RequestMapping("/OOP_Two")
+    public String oopTwo() {
+        return "oopConcept2";
+    }
+    @RequestMapping("/HTML_One")
+    public String htmlOne() {
+        return "htmlTopic1";
+    }
+    @RequestMapping("/CSS_One")
+    public String cssOne() {
+        return "cssTopic1";
+    }
+
 
 
 
