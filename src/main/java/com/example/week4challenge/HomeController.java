abda85bf9877abc787ae785b4baad17ebfeb7b1d
sunnycal <milana.campbell@gmail.com>
2019-05-10 10:38:08 -0400

    controller

--- src/main/java/com/example/week4challenge/HomeController.java
+++ src/main/java/com/example/week4challenge/HomeController.java
@@ -1,4 +1,32 @@
 package com.example.week4challenge;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage(){
+        return "index";
+    }
+
+    @RequestMapping("/profile")
+    public String profilePage() {
+        return "profile";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        return "about";
+
+    }
+
+    @RequestMapping("/git_One")
+    public String aboutGitOne() {
+        return "gitOne";
+
+    }
+
+
 }
